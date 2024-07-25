db085642b9c856539385bb9b20d6b71bb0285c4e
Webskitters <vikashofficial9@gmail.com>
2024-07-25 13:47:44 +0530

    Added Burnable

--- AtaSC.sol
+++ AtaSC.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ArcaneTowerAscendent is ERC721URIStorage, Ownable {
+contract ArcaneTowerAscendent is ERC721URIStorage, ERC721Burnable, Ownable {
     uint256 public tokenCounter;
 
     constructor(address initialOwner) ERC721("Arcane", "ATA") Ownable(initialOwner) {
